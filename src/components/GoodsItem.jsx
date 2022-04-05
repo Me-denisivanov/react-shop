b174c1446754449f3c4c1f607bfd60092e5d1b8a
Денис Иванов <denisweb@MacBook-Air-Denis.local>
2022-04-05 16:43:20 +0300

    first commit

---
+++ src/components/GoodsItem.jsx
@@ -0,0 +1,43 @@
+function GoodsItem(props) {
+  const {
+    id,
+    name,
+    description,
+    price,
+    full_background,
+    addToBasket = Function.prototype,
+  } = props;
+
+  const handleClick = () => {};
+
+  return (
+    <div className="card">
+      <div className="card-image">
+        <img src={full_background} alt={name} />
+      </div>
+      <div className="card-content">
+        <span className="card-title">{name}</span>
+        <p>{description}</p>
+      </div>
+      <div className="card-action">
+        <button
+          className="btn"
+          onClick={() =>
+            addToBasket({
+              id,
+              name,
+              price,
+            })
+          }
+        >
+          Купить
+        </button>
+        <span className="right" style={{ fontSize: "1.4rem" }}>
+          {price} грн
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export { GoodsItem };
